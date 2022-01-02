b73f7a68ca607683f0bf586ca3b3bfd316f8fb3b
LoganM <logan@peacefullane.com>
2022-01-02 10:35:41 -0800

    initial commit

--- arcadeShip/GameScene.swift
+++ arcadeShip/GameScene.swift
@@ -7,82 +7,144 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var player:SKSpriteNode!
+    
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0{
+        didSet{
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var gameTimer:Timer!
+    
+    var possibleAliens = ["enemy", "enemy Red", "enemy Green"]
+    
+    let alienCategory:UInt32 = 0x1 << 1
+    let photonTorpedoCategory:UInt32 = 0x1 << 0
+    
+    let motionManager = CMMotionManager()
+    
+    var xAcceleration:CGFloat = 0
     
     override func didMove(to view: SKView) {
+        player = SKSpriteNode(imageNamed:"RocketShip")
+        player.scale(to: CGSize(width: (scene!.size.height)/(18), height: (scene!.size.height)/(18)))
+        player.position = CGPoint( x:0, y: (scene!.size.height)/(-2.25))
+        player.zPosition = 10
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        scene?.addChild(player)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: (scene!.size.width)/(-2.8), y: (scene!.size.height)/(2.2))
+        scoreLabel.zPosition = 100
+        scoreLabel.fontName = "Arial"
+        scoreLabel.fontSize = 40
+        scene?.addChild(scoreLabel)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error:Error?) in
+            if let accelerometerData = data{
+                let acceleration = accelerometerData.acceleration
+                self.xAcceleration = acceleration.x * 0.75 + self.xAcceleration * 0.25
+            }
+        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    @objc func addAlien() {
+        possibleAliens = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleAliens) as! [String]
+        let alien = SKSpriteNode(imageNamed: possibleAliens[0])
+        alien.scale(to: CGSize(width: (scene!.size.height)/(16), height: (scene!.size.height)/(18)))
+        let randomAlienPosition = GKRandomDistribution(lowestValue: Int((scene!.size.width)/(-2)), highestValue: Int((scene!.size.width)/(2)))
+        
+        alien.position = CGPoint(x: CGFloat(randomAlienPosition.nextInt()), y: self.frame.size.height + alien.size.height)
+        
+        alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
+        alien.physicsBody?.isDynamic = true
+        
+        alien.physicsBody?.categoryBitMask = alienCategory
+        alien.physicsBody?.contactTestBitMask = photonTorpedoCategory
+        alien.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(alien)
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.move(to: CGPoint(x: alien.position.x, y: scene!.size.height/(-2)), duration: 8))
+        actionArray.append(SKAction.removeFromParent())
+        
+        alien.run(SKAction.sequence(actionArray))
     }
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        fireBullet()
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    @objc func fireBullet(){
+        
+        let bulletNode = SKSpriteNode(imageNamed: "Bullet Player")
+        bulletNode.position=player.position
+        bulletNode.position.y += (player.size.height/2)
+        
+        bulletNode.scale(to: CGSize(width: (scene!.size.height)/(36), height: (scene!.size.height)/(36)))
+        
+        bulletNode.physicsBody = SKPhysicsBody(rectangleOf: bulletNode.size)
+        bulletNode.physicsBody?.isDynamic = true
+        bulletNode.physicsBody?.categoryBitMask = photonTorpedoCategory
+        bulletNode.physicsBody?.contactTestBitMask = alienCategory
+        bulletNode.physicsBody?.collisionBitMask = 0
+        bulletNode.physicsBody?.usesPreciseCollisionDetection = true
+        
+        self.addChild(bulletNode)
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.move(to: CGPoint(x: bulletNode.position.x, y: scene!.size.height/(2)), duration: 0.8))
+        actionArray.append(SKAction.removeFromParent())
+        
+        bulletNode.run(SKAction.sequence(actionArray))
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody:SKPhysicsBody
+        var secondBody:SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask{
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        }
+        else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        if (firstBody.categoryBitMask & photonTorpedoCategory) != 0 && (secondBody.categoryBitMask & alienCategory) != 0{
+            torpedoDidCollideWithAlien(bulletNode: firstBody.node as! SKSpriteNode, alienNode: secondBody.node as! SKSpriteNode)
+        }
+    }
+    @objc func torpedoDidCollideWithAlien(bulletNode:SKSpriteNode, alienNode:SKSpriteNode){
+        bulletNode.removeFromParent()
+        alienNode.removeFromParent()
+        score += 10
+    }
+    
+    override func didSimulatePhysics() {
+        player.position.x += xAcceleration * self.size.width/30
+        if(player.position.x < self.size.width/(-2)){
+            player.position.x = self.size.width/(-2)
+        }
+        if(player.position.x > self.size.width/(2)){
+            player.position.x = self.size.width/(2)
+        }
     }
 }
